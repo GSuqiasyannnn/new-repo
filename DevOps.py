44b382bbf66a9276d20805f7b29f111b45fd3453
GSuqiasyannnn <gorsukiasyan72@gmail.com>
2025-02-20 12:22:09 +0400

    Add files via upload

---
+++ DevOps.py
@@ -0,0 +1,35 @@
+# #EX_1
+# _________________________________________
+# number = int(input("Give your integer "))
+# first_num = number // 10
+# second_num = number % 10
+# my_input = (first_num + second_num)
+# print(my_input)
+
+# # EX_2
+# #__________________________________________
+# Weight = (int(input("Pls give your weight ")))
+# print(f"Your weight is {Weight}" 'kg')
+# Height = (int(input("Pls give your height ")))
+# print(f"Your height is {Height}" 'cm')
+# BMI = Weight / Height ** 2
+# print(f"Your BMI is {BMI} ")
+
+
+# #EX_3
+# #_____________________________________________
+# MY_AGE = int(input("Pls Give your age = "))
+# MAX = 90
+# FULL_WEEKS_FOR_YEAR = 52
+# Result = (MAX - FULL_WEEKS_FOR_YEAR) * MY_AGE
+# print(f"You have {Result} weeks for your all life")
+
+
+##EX_4
+#_________________________________________________
+# SUMMA_OF_MONEY = int(input("Pls give your money count "))
+# PEOPLE_COUNT = int(input("How many people you have "))
+# PERCENT = int(input("What % You want to give? "))
+# RESULT = (SUMMA_OF_MONEY / PEOPLE_COUNT) * PERCENT % 100
+# print(f"You must to give {RESULT}$")
+#
