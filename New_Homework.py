efed8d5821d5480d6f1a9e7b02d24d61ac736c84
GSuqiasyannnn <gorsukiasyan72@gmail.com>
2025-02-24 17:52:34 +0400

    Add files via upload

---
+++ New_Homework.py
@@ -0,0 +1,60 @@
+
+#EX-1
+#________________________________________________
+#NUMBER = int(input("Pls give your integer = "))
+# if NUMBER % 2 == 0:
+#     print("This is a even number")
+# else:
+#     print("This is a odd number")
+
+
+# # EX_2
+# #__________________________________________
+# Weight = (int(input("Pls give your weight ")))
+# print(f"Your weight is {Weight}" 'kg')
+# Height = (float(input("Pls give your height ")))
+# print(f"Your height is {Height}" 'cm')
+# BMI = Weight / Height ** 2
+# if 18.5 <=  BMI <= 25:
+#     print(f"Your BMI is {BMI} you have a normal weight ")
+# elif 25 <= BMI <= 30:
+#     print(f"Your BMI is {BMI} you have a slightly overweight ")
+# elif BMI > 30:
+#     print(f"Your BMI is {BMI} you are fat ")
+
+
+#Ex_4
+#_______________________________________________
+# size1 = "S"
+# size2 = "M"
+# size3 = "L"
+# money = 0
+# Order = input("Hi! What size pizza do you want? ")
+# Peperoni = input("Do you want pepperoni? ")
+# Cheese = input("Do you want cheese? ")
+# if Order == size1:
+#     money += 15
+#     print(f"You must pay {money}$")
+# elif Order == size2:
+#     money += 20
+#     print(f"You must pay {money}$")
+# elif Order == size3:
+#     money += 25
+#     print(f"You must pay {money}$")
+# else:
+#     print("We dont have that size: ")
+# if Peperoni == "Yes":
+#     if Order == size1:
+#         money += 2
+#     elif Order == size2 and Order == size3:
+#         money += 3
+#     else:
+#         print("You dont want pepperoni")
+# if Cheese == "Yes":
+#     money += 1
+# else:
+#     print("You dont want cheese")
+# print(f"Your pizza is {Order} size, you must pay {money}$")
+
+
+
